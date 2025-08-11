bad888a7b067029aef6eea8a224ab14743b87dc4
Sayan Basani <sayanbasani1@gmail.com>
2025-08-12 00:07:08 +0530

    backend url

--- WebScraper_forntend/src/config.tsx
+++ WebScraper_forntend/src/config.tsx
@@ -1,1 +1,2 @@
-export const backend_Url = "http://localhost:8000";  // FastAPI port
+// export const backend_Url = "http://localhost:8000";  // FastAPI port
+export const backend_Url = "https://webscraper-backend-h7dc.onrender.com";  // FastAPI deployed url
