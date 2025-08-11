20ac1a85a9d4ada1b9cd02f158a8a9ba3d210877
Sayan Basani <sayanbasani1@gmail.com>
2025-08-12 00:50:32 +0530

    cors

--- WebScraper_forntend/src/config.tsx
+++ WebScraper_forntend/src/config.tsx
@@ -1,2 +1,2 @@
-// export const backend_Url = "http://localhost:8000";  // FastAPI port
-export const backend_Url = "https://webscraper-backend-h7dc.onrender.com";  // FastAPI deployed url
+export const backend_Url = "http://localhost:8000";  // FastAPI port
+// export const backend_Url = "https://webscraper-backend-h7dc.onrender.com";  // FastAPI deployed url
